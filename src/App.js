082eddfb32fca9bc0ff1aa8658f1ab9ffc6fc500
TBill06 <bill.tushar2@gmail.com>
2023-01-30 10:19:00 -0330

    Update Flow

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import Navbar from './components/Navbar.js'
 import About from './components/About.js'
 import Projects from './components/Projects.js'
 import Connect from './components/Connect.js'
-//import { ParallaxProvider } from 'react-scroll-parallax';
+
 
 const App = () => {
 
   return (
-    //<ParallaxProvider>
-    <div className='bg-[#0B192E]'>
+    <div>
       <head><title>Tushar Bill</title></head>
       <Navbar />
       <main className='min-h-screen px-5 md:px-20 lg:px-40'>
         <section className='min-h-screen'>
-          <div className='mb-20'>
-            <div className='flex justify-center items-center'>
-            <h2 className="text-6xl py-2 text-[#58DFC3] font-medium md:text-6xl lg:text-9xl">Hello!</h2>
-            <span className='text-6xl text-[#58DFC3] px-5 hover:scale-x-150 hover:duration-300'>——</span>
-            <h2 className='text-6xl py-2 text-[#58DFC3] font-medium md:text-6xl lg:text-9xl'>My Name is</h2>
-            </div>
-          <h2 className="text-6xl pt-2 pb-7 text-[#58DFC3] font-medium md:text-6xl lg:text-9xl">Tushar!</h2>
-          <p className='text-white text-xl'>Thanks for visiting my portfolio. I'm a Computer Science and Business Student at Memorial University of Newfoundland in St. John's, Canada. I love to build applications which integrate tech into our lives to make it better.</p>
+          <div className='mb-9'>
+          <h2 className="text-5xl py-2 pb-10 text-[#58DFC3] md:text-5xl lg:text-9xl font-normal text-center">Hello — I'm Tushar!</h2>
+          <p className='text-[#CCD6F6] sm:text-base md:text-lg lg:text-xl'>Thanks for visiting my portfolio! I'm a Computer Science and Business Student at Memorial University of Newfoundland in St. John's, Canada. I love to build applications which integrate tech into our lives to make it better.</p>
           </div>
           <About />
           <Projects />
@@ -29,8 +23,6 @@ const App = () => {
         <footer className='flex justify-center items-center pb-7 text-white font-code'>Designed & Built by Tushar!</footer>
       </main>
     </div>
-   //</ParallaxProvider>
   );
 }
-
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,15 @@ const About = () => {
         <div className="pb-6">
         <h3 className="text-4xl text-[#CCD6F6] py-2 md:text-4xl">Experience:</h3>
         <div className="p-5">
+        <h4 className="text-[#58DFC3] text-xl pb-1 md:text-2xl">Full Stack Developer</h4>
+        <h4 className="text-[#58DFC3] text-xl md:text-2xl">@ DIAG Lab - Memorial University</h4>
+        <ul className="list-disc text-white text-xl px-6 pt-3 list-inside">
+            <li>Worked in a team developing issue/ticket management system for a machine learning lab detecting medical images.</li>
+        </ul>
+        </div>
+        <div className="p-5">
         <h4 className="text-[#58DFC3] text-xl pb-1 md:text-2xl">Learning Design and Development Assistant</h4>
-        <h4 className="text-[#58DFC3] text-xl md:text-2xl">@ CITL.</h4>
+        <h4 className="text-[#58DFC3] text-xl md:text-2xl">@ CITL - Memorial University</h4>
         <ul className="list-disc text-white text-xl px-6 pt-3 list-inside">
             <li>Developed web pages for nursing courses which were taught online at MUN Nursing school</li>
             <li>Wrote technical reports for support page release notes.</li>
